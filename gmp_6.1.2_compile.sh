4e8e2ccc89564c4f0b8ac5dcce181c3c26c6ee18
mikhail-j <mikhail.jin@gmail.com>
2017-07-17 07:41:35 -0400

    Modified gpg signature verification into a function.

--- gmp_6.1.2_compile.sh
+++ gmp_6.1.2_compile.sh
@@ -23,40 +23,16 @@ found_curl_exit_code=$?
 found_gpg=$(command -v gpg >/dev/null 2>&1)
 found_gpg_exit_code=$?
 
-gpg_verify_output=
 gmp_tar_verified=false
 
-
-if test $found_curl_exit_code -eq 0; then
-	curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
-	if test $? -ne 0; then
-		exit 1
-	fi
-
-	curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2.sig
-	if test $? -ne 0; then
-		exit 1
-	fi
-
-	if test $found_gpg_exit_code -eq 0; then
-		if test -f gmp-6.1.2.tar.bz2 && test -f gmp-6.1.2.tar.bz2; then
-			gpg_verify_output=$(gpg --status-fd 1 --verify gmp-6.1.2.tar.bz2.sig)
-		fi
-	else
-		echo "error: gpg could not be found!"
-		exit 1
-	fi
-else
-	echo "error: curl could not be found!"
-	exit 1
-fi
-
-case $gpg_verify_output in
+verify_archive()
+{
+case $1 in
 	*"GOODSIG"*)
-		case $gpg_verify_output in
+		case $1 in
 			*"VALIDSIG"*)
 				# Check for fingerprint from https://gmplib.org/
-				case $gpg_verify_output in
+				case $1 in
 					*"343C2FF0FBEE5EC2EDBEF399F3599FF828C67298"*)
 						gmp_tar_verified=true
 					;;
@@ -65,11 +41,86 @@ case $gpg_verify_output in
 		esac
 	;;
 esac
+}
+
+# Verify files immediately if they already exist.
+if test $found_gpg_exit_code -eq 0; then
+	if test -f gmp-6.1.2.tar.bz2 && test -f gmp-6.1.2.tar.bz2; then
+		verify_archive $(gpg --status-fd 1 --verify gmp-6.1.2.tar.bz2.sig)
+	fi
+else
+	echo "error: gpg could not be found!"
+	exit 1
+fi
+
+# The following runs when we need a new download of both the archive
+# and its respective signature.
+if ! $gmp_tar_verified; then
+	if test $found_curl_exit_code -eq 0; then
+		curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
+		if test $? -ne 0; then
+			exit 1
+		fi
+	
+		curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2.sig
+		if test $? -ne 0; then
+			exit 1
+		fi
+	
+		if test $found_gpg_exit_code -eq 0; then
+			if test -f gmp-6.1.2.tar.bz2 && test -f gmp-6.1.2.tar.bz2; then
+				verify_archive $(gpg --status-fd 1 --verify gmp-6.1.2.tar.bz2.sig)
+			fi
+		else
+			echo "error: gpg could not be found!"
+			exit 1
+		fi
+	else
+		echo "error: curl could not be found!"
+		exit 1
+	fi
+fi
+
+# Remove directory from previous compilation attempt (if gmp-6.1.2 already exists).
+if test -d gmp-6.1.2; then
+	rm -rf gmp-6.1.2
+fi
 
 if $gmp_tar_verified; then
 	tar -xjf gmp-6.1.2.tar.bz2
+	if test $? -ne 0; then
+		exit $?
+	fi 
 else
 	echo "error: gmp-6.1.2.tar.bz2 could not be verified!"
 	exit 1
 fi
 
+if test -d gmp-6.1.2; then
+	cd gmp-6.1.2
+else
+	echo "error: The gmp-6.1.2 folder does not exist!"
+	exit 1
+fi
+
+if test -f configure; then
+	./configure --enable-shared --enable-static --prefix=/usr/local
+	if test $? -ne 0; then
+		exit $?
+	fi
+else
+	echo "error: configure was not found!"
+	exit 1
+fi
+
+make -j 8
+if test $? -ne 0; then
+	exit $?
+fi
+
+make check
+if test $? -ne 0; then
+	exit $?
+fi
+
+sudo make install
