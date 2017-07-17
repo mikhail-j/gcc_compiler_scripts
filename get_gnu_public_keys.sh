7f1778db1a8c348814e3f70f8904b426eefaf4da
mikhail-j <mikhail.jin@gmail.com>
2017-07-17 06:46:04 -0400

    Fixed variable naming to lowercase (prevent confusion with environmental variables. Updated file header comments.

--- get_gnu_public_keys.sh
+++ get_gnu_public_keys.sh
@@ -1,38 +1,39 @@
-#!/bin/sh
+#! /bin/sh
+# Get and import the public keys from the latest GNU project GPG keyring.
+#
 # Copyright (C) 2017 Qijia (Michael) Jin
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-#
-#
-# Get and import the public keys from the latest GNU project GPG keyring.
-#
 
-FOUND_CURL=$(command -v curl >/dev/null 2>&1)
-FOUND_CURL_EXIT_CODE=$?
+found_curl=$(command -v curl >/dev/null 2>&1)
+found_curl_exit_code=$?
 
-FOUND_GPG=$(command -v gpg >/dev/null 2>&1)
-FOUND_GPG_EXIT_CODE=$?
+found_gpg=$(command -v gpg >/dev/null 2>&1)
+found_gpg_exit_code=$?
 
 
-if test $FOUND_CURL_EXIT_CODE -eq 0; then
+if test $found_curl_exit_code -eq 0; then
 	# This script is intended to download the latest version
 	# of the GNU keyring file and import the public keys.
 	curl -O ftp://ftp.gnu.org/pub/gnu/gnu-keyring.gpg
-	if test $? -eq 0 && test $FOUND_GPG_EXIT_CODE -eq 0; then
-		gpg --import gnu-keyring.gpg
+	if test $? -eq 0; then
+		if test $found_gpg_exit_code -eq 0; then
+			gpg --import gnu-keyring.gpg
+		else
+			echo "error: gpg could not be found!"
+		fi
 	fi
 else
 	echo "error: curl could not be found!"
--- gmp_6.1.2_compile.sh
+++ gmp_6.1.2_compile.sh
@@ -1,41 +1,46 @@
-#!/bin/sh
+#! /bin/sh
+# Compile and install GMP v6.1.2.
+#
 # Copyright (C) 2017 Qijia (Michael) Jin
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-#
-#
-# Compile and install GMP v6.1.2.
-#
 
-FOUND_CURL=$(command -v curl >/dev/null 2>&1)
-FOUND_CURL_EXIT_CODE=$?
+found_curl=$(command -v curl >/dev/null 2>&1)
+found_curl_exit_code=$?
 
-FOUND_GPG=$(command -v gpg >/dev/null 2>&1)
-FOUND_GPG_EXIT_CODE=$?
+found_gpg=$(command -v gpg >/dev/null 2>&1)
+found_gpg_exit_code=$?
 
-GPG_VERIFY_OUTPUT=
-GMP_TAR_VERIFIED=
+gpg_verify_output=
+gmp_tar_verified=false
 
 
-if test $FOUND_CURL_EXIT_CODE -eq 0; then
+if test $found_curl_exit_code -eq 0; then
 	curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
+	if test $? -ne 0; then
+		exit 1
+	fi
+
 	curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2.sig
-	if test $FOUND_GPG_EXIT_CODE -eq 0; then
+	if test $? -ne 0; then
+		exit 1
+	fi
+
+	if test $found_gpg_exit_code -eq 0; then
 		if test -f gmp-6.1.2.tar.bz2 && test -f gmp-6.1.2.tar.bz2
-			GPG_VERIFY_OUTPUT=$(gpg --verify gmp-6.1.2.tar.bz2.sig 2>&1)
+			gpg_verify_output=$(gpg --status-fd 1 --verify gmp-6.1.2.tar.bz2.sig)
 		fi
 	else
 		echo "error: gpg could not be found!"
@@ -46,17 +51,22 @@ else
 	exit 1
 fi
 
-case "Good signature" in
-	$GPG_VERIFY_OUTPUT)
-	case "343C 2FF0 FBEE 5EC2 EDBE F399 F359 9FF8 28C6 7298" in
-		$GPG_VERIFY_OUTPUT)
-		GMP_TAR_VERIFIED=true
-		;;
-	esac
+case "GOODSIG" in
+	$gpg_verify_output)
+		case "VALIDSIG" in
+			$gpg_verify_output)
+				# Check for fingerprint from https://gmplib.org/
+				case "343C2FF0FBEE5EC2EDBEF399F3599FF828C67298" in
+					$gpg_verify_output)
+					gmp_tar_verified=true
+					;;
+				esac
+			;;
+		esac
 	;;
 esac
 
-if test $GMP_TAR_VERIFIED == true; then
+if $gmp_tar_verified; then
 	tar -xjf gmp-6.1.2.tar.bz2
 else
 	echo "error: gmp-6.1.2.tar.bz2 could not be verified!"
